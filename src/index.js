a1c50e1e995e5ef2c5cf97bdb293344fb6886ca2
dave krumland <axolotlman2@gmail.com>
2021-07-05 17:16:29 -0700

    test

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ const si = require('systeminformation');
 
 
 
-
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
